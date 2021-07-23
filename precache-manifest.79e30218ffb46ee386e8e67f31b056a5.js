b46d931a7d44923c67c076c929d716869fb1868c
John <>
2021-07-23 16:05:38 -0400

    Updates

---
+++ precache-manifest.79e30218ffb46ee386e8e67f31b056a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "588cb692f0a3be2a5857a85d189898c3",
+    "url": "/monstersapp/index.html"
+  },
+  {
+    "revision": "016cc159854b0fc74a02",
+    "url": "/monstersapp/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monstersapp/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstersapp/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "016cc159854b0fc74a02",
+    "url": "/monstersapp/static/js/main.0fe4c3cc.chunk.js"
+  },
+  {
+    "revision": "baed90816a749f94ce44",
+    "url": "/monstersapp/static/js/runtime-main.475b659a.js"
+  }
+]);
